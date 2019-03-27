13587cf6f460c831b4f62a06c59b27869c70379e
Ismael Nascimento <contato@ismaelnascimento.com>
2019-03-27 02:32:12 -0300

    initial commit

---
+++ VuforiaMultiTrack/Assets/_Project/Scripts/AppManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    public static AppManager Instance;
+    [SerializeField] private GameObject capsule;
+    public GameObject Capsule
+    {
+        get { return capsule; }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
---
+++ VuforiaMultiTrack/Assets/_Project/Scripts/Fusion.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Fusion : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Cube")
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            other.GetComponent<MeshRenderer>().enabled = false;
+            AppManager.Instance.Capsule.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Cube")
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            other.GetComponent<MeshRenderer>().enabled = true;
+            AppManager.Instance.Capsule.SetActive(false);
+        }
+    }
+
+    /*
+   private void OnTriggerStay(Collider other)
+   {
+       Debug.LogFormat("OnTriggerStay::\n Sou {0} e estou colidindo com {1}", gameObject.name, other.name);
+   }
+
+   private void OnCollisionEnter(Collision collision)
+   {
+       Debug.LogFormat("OnCollisionEnter::\n Sou {0} e colidir com {1}", gameObject.name, collision.gameObject.name);
+
+   }
+
+   private void OnCollisionExit(Collision collision)
+   {
+       Debug.LogFormat("OnCollisionExit::\n Sou {0} e colidir com {1}", gameObject.name, collision.gameObject.name);
+
+   }
+
+   private void OnCollisionStay(Collision collision)
+   {
+       Debug.LogFormat("OnCollisionStay::\n Sou {0} e colidir com {1}", gameObject.name, collision.gameObject.name);
+
+   }
+   */
+}
